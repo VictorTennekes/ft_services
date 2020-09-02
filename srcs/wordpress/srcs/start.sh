8ab2dd82db8af65cc98bf4e2c0cf9f0da1772af2
Victor Tennekes <victor@tennekes.nl>
2020-09-02 14:57:35 +0200

    Fix: un-hardcoded some stuff

--- srcs/wordpress/srcs/start.sh
+++ srcs/wordpress/srcs/start.sh
@@ -4,7 +4,7 @@ chown -R www:www /www
 
 cp /tmp/wp-config.php /www/wp-config.php
 
-envsubst '${WORDPRESS_SVC_SERVICE_HOST} ${WORDPRESS_SVC_SERVICE_PORT} ${DB_NAME} ${DB_USER} ${DB_PASS} ${DB_HOST}' < /tmp/wp-config.php > /www/wp-config.php
+envsubst '${DB_NAME} ${DB_USER} ${DB_PASSWORD} ${DB_HOST}' < /tmp/wp-config.php > /www/wp-config.php
 rm /tmp/wp-config.php
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
--- srcs/wordpress/srcs/wp-config.php
+++ srcs/wordpress/srcs/wp-config.php
@@ -7,7 +7,7 @@ define( 'DB_NAME', '${DB_NAME}' );
 define( 'DB_USER', '${DB_USER}' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '${DB_PASS}');
+define( 'DB_PASSWORD', '${DB_PASSWORD}' );
 
 /** MySQL hostname */
 define( 'DB_HOST', '${DB_HOST}' );
